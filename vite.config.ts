dfa0b64111449833928272eee6f68e7947b76333
enomotoryota <enomoito@gmail.com>
2024-05-08 14:04:07 +0900

    git yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio2024/",
+  base: "/portfolio20242/",
   plugins: [react()],
 });
